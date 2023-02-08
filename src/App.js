764d8db761d56780e61a5b2f0f1f6136945bd7e0
oandeliz <59943720+oandeliz@users.noreply.github.com>
2023-02-08 18:39:54 -0500

    simple cal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Center, Square, Text, Box } from '@chakra-ui/react'
+import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +32,70 @@ function App() {
         </a>
       </header>
     </div>
+    <div>
+    <Flex color='white'>
+      <Center flex='1' bg='green.500'>
+      <NumberInput defaultValue={15} min={10} max={20}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      </Center>
+      <Center flex='1' bg='green.500'>
+        <AddIcon />
+      </Center>
+      <Center flex='1' bg='green.500'>
+      <NumberInput defaultValue={15} min={10} max={20}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      </Center>
+      <Center flex='1' bg='green.500'>
+        <Text> = </Text>
+      </Center>
+      <Center flex='2' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+    </Flex>
+
+    <Flex color='white'>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Square bg='blue.500' size='150px'>
+        <Text>Box 2</Text>
+      </Square>
+      <Box flex='1' bg='tomato'>
+        <Text>Box 3</Text>
+      </Box>
+    </Flex>
+
+
+
+
+      
+    </div>
+    </ChakraProvider>
   );
 }
 

24669cdee08fd9d1ac7bf3e07d798dcce9c0e686
oandeliz <59943720+oandeliz@users.noreply.github.com>
2023-04-01 22:41:36 -0400

    added components and an add function

--- src/App.js
+++ src/App.js
@@ -12,20 +12,21 @@ import { Flex, Spacer } from '@chakra-ui/react'
 import { Center, Square, Text, Box } from '@chakra-ui/react'
 import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
 import CenteredText from './components/CenteredText'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const MIN = 0
 const MAX = 20
 
 function App() {
-  const [leftNumber, setLeftNumber]  = useState(1)
+  const [leftNumber, setLeftNumber]  = useState(null)
   const [rightNumber, setRightNumber]  = useState(2)
+  const lineItemTotal = leftNumber + rightNumber 
   return (
     <ChakraProvider><div>
     <Flex color='white'>
       <Center flex='1' bg='green.500'>
-        <NumberInput defaultValue={leftNumber} min={MIN} max={MAX}
-        onChange={(i)=> setLeftNumber(i)}
+        <NumberInput defaultValue={leftNumber} max={MAX}
+        onChange={(i)=> setLeftNumber(+i)}
         >
           <NumberInputField />
           <NumberInputStepper>
@@ -38,7 +39,8 @@ function App() {
         <AddIcon />
       </Center>
       <Center flex='1' bg='green.500'>
-        <NumberInput defaultValue={rightNumber} min={MIN} max={MAX}>
+        <NumberInput defaultValue={rightNumber} min={MIN} max={MAX}
+        onChange={(i)=> setRightNumber(Number(i))}>
           <NumberInputField />
           <NumberInputStepper>
             <NumberIncrementStepper />
@@ -50,7 +52,7 @@ function App() {
         <Text> = </Text>
       </Center>
       <Center flex='2' bg='green.500'>
-        <Text>Box 1</Text>
+        <Text>{ lineItemTotal }</Text>
       </Center>
     </Flex>
 
